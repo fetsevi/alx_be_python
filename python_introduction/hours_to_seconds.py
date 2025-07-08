1441682c7a7950d0bf8c3a602568b0328355e08d
fetsevi <etsevifrancois@gmail.com>
2025-07-08 12:19:55 +0000

    Add of rectangle_area.py and hours_to_seconde.py file

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,12 @@
+# Define variables
+
+hours = 2
+
+# Hours to seconds operation
+
+seconds = hours * 3600
+
+# Print result
+
+print(hours, " hour(s) is ", seconds, " seconds. ")
+
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+# Variable declaration
+
+length = 10
+width = 5
+
+# Area of rectangle operation
+
+area = length * width
+
+# Print result
+
+print(" The area of the rectangle is: ", area)
