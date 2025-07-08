fd67279d5ab6d39e92d927fab11823d397585dcf
fetsevi <etsevifrancois@gmail.com>
2025-07-08 09:47:54 +0000

    Add of python basic operation file and folder

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# Define variables
+
+number1 = 10
+number2 = 5
+
+# Perform operations
+
+addition = number1 + number2
+substraction = number1 - number2
+multiplication = number1 * number2
+
+# Print results
+
+print(" Addition of ", number1, " and ", number2, " is ", addition)
+print(" Substraction of ", number1, " and ", number2, " is ", substraction)
+print(" Multiplication of ", number1, " and ", number2, " is ", multiplication)
+
