a5b4faae82a600f4a32a0447e11433292a05922b
fetsevi <etsevifrancois@gmail.com>
2025-07-08 11:49:49 +0000

    Add of calculator file simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+# Variable declaration
+
+principal = 1000
+rate = 0.05
+time = 3
+
+# Simple interest operation 
+
+interest = principal * rate * time
+
+# Print result
+
+print(" The simple interest is: ", interest)
+
