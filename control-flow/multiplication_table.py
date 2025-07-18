c5fdeefb895d3e0fe263341ad09fc9de701448e2
fetsevi <etsevifrancois@gmail.com>
2025-07-18 09:49:08 +0000

    Add of range to 10

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -5,5 +5,5 @@ number=int(input("Enter a number to see its multiplication table: "))
 
 # loop boocle to print multiplication table
 
-for n in range(1, 10):
+for n in range(1, 11):
     print(f"{number} * {n} = {number*n}")
