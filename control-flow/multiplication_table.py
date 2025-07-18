24dd0e64d97be1e4b9dced0e04af8f4bd72c5245
fetsevi <etsevifrancois@gmail.com>
2025-07-18 09:35:44 +0000

    Add of multiplication table filein control flow folder

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+
+# Variable define
+
+number=int(input("Enter a number to see its multiplication table: "))
+
+# loop boocle to print multiplication table
+
+for n in range(1, 10):
+    print(f"{number} * {n} = {number*n}")
