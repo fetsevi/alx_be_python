a5e6c0c8e3315106b6e0517e7d114cbbcec3ac38
fetsevi <etsevifrancois@gmail.com>
2025-07-23 16:09:41 +0000

    Add of shopping file in the function and data folder

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+# Display menu function
+
+def display_menu():
+    print("*******Shopping List manager*******")
+    print("Make your choice: ")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+# Define main function
+
+def main():
+    shopping_list=[]  # List declaration
+    while True:
+        display_menu()  # call of display_menu function
+        choice=input("Enter your choice: ")
+        if choice=='1':
+            add_item=input("Item name to add: ")
+            shopping_list.append(add_item)
+        elif choice=='2':
+            remove_item=input("Enter item to remove: ")
+            if remove_item in shopping_list:
+                    shopping_list.remove(remove_item)
+            else: print("Cannot find", remove_item, "in the shopping list\n")
+        
+        elif choice=='3':
+            print(f"\n {shopping_list} \n")
+        elif choice=='4':
+            print("Goodbye")
+            break
+        else: print("Invalid choice. please try again")
+
+if __name__== "__main__":
+    main()
+
