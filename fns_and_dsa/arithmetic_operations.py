26530a403a43aaa29fa3d13c4fc0c79cf07f4976
fetsevi <etsevifrancois@gmail.com>
2025-07-23 11:16:16 +0000

    Add of function and data structure folder with extra change

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,8 +7,7 @@ def perform_operation(num1, num2, operation):
     if operation=='add': return num1+num2
     elif operation=='subtract': return num1-num2
     elif operation=='multiply': return num1*num2
-    elif operation=='divide':
-       if num2==0: return print("Error cannot be divided by 0")
-       else: return num1/num2
+    elif num2==0 and operation=='divide': return print("Error cannot be divided by 0")
+    elif num2!=0 and operation=='divide': return num1/num2
     
     else: return print("Unknown operator")
