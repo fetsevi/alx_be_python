ab5a6e65651a7a744a62eec57715b8e925986fc3
fetsevi <etsevifrancois@gmail.com>
2025-07-23 10:38:34 +0000

    Add of function and data structure folder modify

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,9 @@
 # Define arithmetic function
 
-def perform_operation(num1,num2,operation):
+num1=0
+num2=0
+operation=''
+def perform_operation(num1, num2, operation):
     if operation=='add': return num1+num2
     elif operation=='subtract': return num1-num2
     elif operation=='multiply': return num1*num2
