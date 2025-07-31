82eef38147cae848ecee0c43713aa039a92fb917
fetsevi <etsevifrancois@gmail.com>
2025-07-31 16:28:16 +0000

    Add of test file of simple calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,33 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-4, -5), -9)
+    def tes_substraction(self):
+        """Test the substraction method."""
+        self.assertEqual(self.calc.subtract(9, 6), 3)
+        self.assertEqual(self.calc.subtract(-2, 3), -5)
+        self.assertEqual(self.calc.subtract(-5, -7), 2)
+        """Test the multiplication method."""
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 5), 15)
+        self.assertEqual(self.calc.multiply(-3, 4), -12)
+        self.assertEqual(self.calc.multiply(-3, -2), 6)
+    def test_division(self):
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+    def test_division_by_zero(self):
+        self.assertIsNone(self.calc.divide(6, 0))
+        
+#if __name__ == '__main__':
+ #   unittest.main()
