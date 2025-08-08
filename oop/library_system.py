b1c66e8dd2a3078dcd2fc4a539fda8745d6b9da7
fetsevi <etsevifrancois@gmail.com>
2025-08-08 16:25:35 +0000

    Add of library system file modified in oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -10,15 +10,15 @@ class Book:
     
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
-        super().__init__
+        super().__init__(title, author)
         self.file_size = file_size
     
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
     
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
-        super().__init__
+        super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
