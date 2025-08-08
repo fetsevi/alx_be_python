a78cecfee3397f954c0f1ce750589396e5f2061f
fetsevi <etsevifrancois@gmail.com>
2025-08-08 14:51:28 +0000

    Add of magic method in oop folder

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+# MAGIC METHOD PYTHON
+
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """Constructor initializes the book instance"""
+        self.title = title
+        self.author = author
+        self.year = year
+    
+    def __del__ (self):
+        """Destructor called when the book object is deletd"""
+        print(f"Deleting {self.title}")
+    def __str__(self):
+        """String representation for human readable display"""
+        return f"{self.title} by {self.author}, published in {self.year}"
+    def __repr__(self):
+        """Official representation for recreating the object"""
+        return f"Book('{self.title}', '{self.author}', '{self.year}'.)"
